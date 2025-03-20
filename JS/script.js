84389e4920d0130cd3b9778aa6282700bef9e795
Matthew Loyed <matthewloyed@gmail.com>
2025-03-20 08:22:48 -0500

    Initial commit - Add WebApp Files

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+document.getElementById("signupBtn").addEventListener("click", function() {
+    window.location.href = "signup.html";
+});
+
+document.getElementById("homeBtn").addEventListener("click", function() {
+    window.location.href = "home.html";
+});
+
+document.getElementById("loginBtn").addEventListener("click", function() {
+    window.location.href = "login.html";
+});
